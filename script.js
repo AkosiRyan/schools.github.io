c7531bc78d6cf57397b5870ffb5d182607d87b47
Ryan <111445635+AkosiRyan@users.noreply.github.com>
2022-08-18 18:23:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/*toggle menu*/
+
+var navlinks=document.getElementById("navlinks");
+
+function showmenu(){
+    navlinks.style.right = "0";
+}
+function hidemenu(){
+    navlinks.style.right = "-200px";
+}
